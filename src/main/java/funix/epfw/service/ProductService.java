bf2d79cd059d2586b8e726b5bd992cc0292bf344
dainpfx00342 <dainpfx00342@funix.edu.vn>
2025-02-15 00:47:53 +0700

    role to see product

--- src/main/java/funix/epfw/service/ProductService.java
+++ src/main/java/funix/epfw/service/ProductService.java
@@ -16,16 +16,19 @@ public class ProductService {
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
-    //Add product service here
+
+    //Add product service
     public void addProduct(Product product) {
         productRepository.save(product);
     }
 
+    //Get all products by user
     public List<Product> getAllProductsByUser(User currentUser) {
         return productRepository.findProductByCreatedBy(currentUser);
     }
 
+    //Get all products
     public List<Product> findAll() {
-        return productRepository.findAll();
+         return productRepository.findAll();
     }
 }
